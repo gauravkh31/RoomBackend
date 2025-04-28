6348f139a0531a342c0dee0d6a72b1f6f12721a7
gauravkh31 <khandelwalgaurav31@gmail.com>
2025-04-28 21:17:41 +0530

    first commit

---
+++ src/test/java/com/example/backend/RoomBackend/RoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend.RoomBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
